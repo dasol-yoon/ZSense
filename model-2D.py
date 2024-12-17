e9e2dfff150605e78e753070c3184e057b5ca5db
dasol-yoon <dy327@cornell.edu>
2024-12-16 22:02:19 -0500

    Create model-2D.py

---
+++ model-2D.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+#from torch.utils.data import DataLoader, Dataset
+
+class IntoInelastic2D(nn.Module):
+    def __init__(self):
+        super(IntoInelastic2D, self).__init__()
+
+        # Encoder
+        self.enc1 = self.conv_block(2, 64)
+        self.enc2 = self.conv_block(64, 128)
+        self.enc3 = self.conv_block(128, 256)
+
+        # Bottleneck
+        self.bottleneck = self.conv_block(256, 512)
+
+        # Decoder
+        self.dec3 = self.upconv_block(512 + 256, 256)
+        self.dec2 = self.upconv_block(256 + 128, 128)
+        self.dec1 = self.upconv_block(128 + 64, 64)
+
+        # Final output layer
+        self.final = nn.Conv2d(64, 1, kernel_size=1)
+
+    def conv_block(self, in_channels, out_channels):
+        """A convolutional block with two Conv2D layers and ReLU activations."""
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def upconv_block(self, in_channels, out_channels):
+        """An upsampling block with transposed convolution and a conv block."""
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
+            self.conv_block(out_channels, out_channels)
+        )
+
+    def forward(self, x):
+        # Encoder
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(F.max_pool2d(enc1, 2))
+        enc3 = self.enc3(F.max_pool2d(enc2, 2))
+
+        # Bottleneck
+        bottleneck = self.bottleneck(F.max_pool2d(enc3, 2))
+
+        # Decoder
+        dec3 = self.dec3(torch.cat((self.upsample(bottleneck, enc3), enc3), dim=1))
+        dec2 = self.dec2(torch.cat((self.upsample(dec3, enc2), enc2), dim=1))
+        dec1 = self.dec1(torch.cat((self.upsample(dec2, enc1), enc1), dim=1))
+
+        # Final layer
+        final_output = self.final(dec1)
+        return final_output
+
+    def upsample(self, x, target):
+        """Upsample `x` to the size of `target`."""
+        return F.interpolate(x, size=target.shape[2:], mode='bilinear', align_corners=True)
